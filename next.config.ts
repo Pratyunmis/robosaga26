84578fa236d04ef5c4ef7ec54ba18b88bb528c9f
Salil Lakra <salillakra.dev@gmail.com>
2025-12-11 00:25:50 +0530

    feat: Add image remote patterns configuration to Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
